1e6879160c6c33155f299a8e4810695a605f323a
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2019-09-18 22:22:47 +0100

    Updates

---
+++ precache-manifest.4db8b012974a92a8695dfe947cb88954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c098e05093fea530f0d57d75ce9731cc",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "e7ab9bfd0e7a826ba6b0",
+    "url": "/pomodoro/static/css/main.87510eb6.chunk.css"
+  },
+  {
+    "revision": "714b27cdf3503b159808",
+    "url": "/pomodoro/static/js/2.d26470ac.chunk.js"
+  },
+  {
+    "revision": "e7ab9bfd0e7a826ba6b0",
+    "url": "/pomodoro/static/js/main.c6566d5d.chunk.js"
+  },
+  {
+    "revision": "61acc7702867c4b1bb09",
+    "url": "/pomodoro/static/js/runtime~main.0786b0a7.js"
+  },
+  {
+    "revision": "cc22f54899dabe70b07cc313151dc6e6",
+    "url": "/pomodoro/static/media/bg.cc22f548.jpg"
+  }
+]);
